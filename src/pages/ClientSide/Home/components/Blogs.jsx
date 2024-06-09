641c7661ae1e42ceca68f43167487bb64b566404
ashikur <ashikur1602@gmail.com>
2024-06-09 10:46:03 +0600

    Blogs card changed

--- src/pages/ClientSide/Home/components/Blogs.jsx
+++ src/pages/ClientSide/Home/components/Blogs.jsx
@@ -12,6 +12,7 @@ import teacher4 from "../../../../assets/images/teacher4.jpg";
 
 // import required modules
 import { FreeMode, Pagination } from "swiper/modules";
+import { Link } from "react-router-dom";
 
 const Blogs = () => {
   return (
@@ -34,7 +35,7 @@ const Blogs = () => {
             spaceBetween: 40,
           },
           1024: {
-            slidesPerView: 4,
+            slidesPerView: 3,
             spaceBetween: 50,
           },
         }}
@@ -42,134 +43,133 @@ const Blogs = () => {
         className="mySwiper bg-white   "
       >
         <SwiperSlide>
-          <div className="card bg-white shadow-2xl border border-pink-800">
-            <figure className="">
-              <img
-                src="https://hips.hearstapps.com/hmg-prod/images/01-fendi-women-fw-22-23-jpg-1645720448.jpg?crop=1xw:0.33336951801997394xh;center,top&resize=640:*"
-                alt="Shoes"
-                className=" h-44"
-              />
-            </figure>
-            <div className="card-body items-center text-left">
-              <p className="card-title">
-                Summer 2024 Fashion Trends: What to Wear This Season
-              </p>
-              
-              <p>
-                Dive into the hottest trends of Summer 2024 with our
-                comprehensive guide. From bold prints to sustainable fashion,...
-              </p>
-              <div className="card-actions">
-                <div className="flex justify-between gap-5 items-center">
+          <div className="bg-white shadow-md rounded-lg overflow-hidden flex flex-col h-96 mb-10">
+            <div className="h-64 w-full overflow-hidden">
+              <img src="https://hips.hearstapps.com/hmg-prod/images/01-fendi-women-fw-22-23-jpg-1645720448.jpg?crop=1xw:0.33336951801997394xh;center,top&resize=640:*" className="w-full h-full object-cover" />
+            </div>
+            <div className="p-4 flex-grow flex flex-col">
+              <h2 className="text-lg font-bold mb-2">Summer 2024 Fashion Trends: What to Wear This Season</h2>
+              <p className="flex-grow">Dive into the hottest trends of Summer 2024 with our
+                comprehensive guide. From bold prints to sustainable fashion,...</p>
+            </div>
+            <div className="px-10 mx-auto mb-5 flex justify-between gap-5 items-center">
+              <Link to="/blogDetails">
                 <button className="btn bg-pink-700 text-white hover:text-black  active:bg-red-700 focus:outline-none focus:ring focus:ring-red-300 focus:text-white">
                   Read More
-                  </button>
-                  
-                  <p className="text-sm">15 june, 2024</p>
-                </div>
-              </div>
+                </button>
+              </Link>
+
+              <p className="text-sm">6 May, 2024</p>
             </div>
+
           </div>
+
+
+
         </SwiperSlide>
-
-        
-        <SwiperSlide>
-          <div className="card bg-base-100  shadow-2xl border border-pink-800">
-            <figure className="">
-              <img
-                src="https://hips.hearstapps.com/hmg-prod/images/spring-summer-2024-fashion-trends-65fdb5a49a846.jpg?crop=1.00xw:1.00xh;0,0&resize=2048:*"
-                alt="Shoes"
-                className=" h-44"
-              />
-            </figure>
-            <div className="card-body items-center text-left">
-              <p className="card-title">
-                What the NewYork Fashion did: What to Wear This Season
-              </p>
-              
-              <p>
-                Dive into the hottest trends of Summer 2024 with our
-                comprehensive guide. From bold prints to sustainable fashion,...
-              </p>
-              <div className="card-actions">
-                <div className="flex justify-between gap-5 items-center">
-                <button className="btn bg-pink-700 text-white hover:text-black  active:bg-red-700 focus:outline-none focus:ring focus:ring-red-300 focus:text-white">
-                  Read More
-                  </button>
-                  
-                  <p className="text-sm">15 june, 2024</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </SwiperSlide>
-
-
 
         <SwiperSlide>
-          <div className="card bg-base-100  shadow-2xl border border-pink-800">
-            <figure className="">
-              <img
-                src="https://www.boxmark.com/0cache/bilder152_900x600_f_center_middle.jpg"
-                alt="Shoes"
-                className=" h-44"
-              />
-            </figure>
-            <div className="card-body items-center text-left">
-              <p className="card-title">
-                Leather Design Course 
-              </p>
-              
-              <p>
-                Dive into the hottest trends of Summer 2024 with our
-               Lorem ipsum dolor sit amet consectetur adit Lorem ipsum dolor d .
-              </p>
-              <div className="card-actions">
-                <div className="flex justify-between gap-5 items-center">
+          <div className="bg-white shadow-md rounded-lg overflow-hidden flex flex-col h-96">
+            <div className="h-64 w-full overflow-hidden">
+              <img src="https://hips.hearstapps.com/hmg-prod/images/spring-summer-2024-fashion-trends-65fdb5a49a846.jpg?crop=1.00xw:1.00xh;0,0&resize=2048:*" className="w-full h-full object-cover" />
+            </div>
+            <div className="p-4 flex-grow flex flex-col">
+              <h2 className="text-lg font-bold mb-2">Summer 2024 Fashion Trends: What to Wear This Season</h2>
+              <p className="flex-grow">Dive into the hottest trends of Summer 2024 with our
+                comprehensive guide. From bold prints to sustainable fashion,...</p>
+            </div>
+            <div className="px-10 mx-auto mb-5 flex justify-between gap-5 items-center">
+              <Link to="/blogDetails">
                 <button className="btn bg-pink-700 text-white hover:text-black  active:bg-red-700 focus:outline-none focus:ring focus:ring-red-300 focus:text-white">
                   Read More
-                  </button>
-                  
-                  <p className="text-sm">15 june, 2024</p>
-                </div>
-              </div>
+                </button>
+              </Link>
+
+              <p className="text-sm">6 May, 2024</p>
             </div>
+
           </div>
+
+
+
         </SwiperSlide>
-
-
 
         <SwiperSlide>
-          <div className="card bg-base-100  shadow-2xl border border-pink-800">
-            <figure className="">
-              <img
-                src="https://cdn.dribbble.com/userupload/8647640/file/original-4215771ec4810cd6ecf1485df5d55d96.png?resize=400x300&vertical=center"
-                alt="Shoes"
-                className=" h-44"
-              />
-            </figure>
-            <div className="card-body items-center text-left">
-              <p className="card-title">
-                Pattern Design Course
-              </p>
-              
-              <p>
-                Dive into the hottest trends o our
-                comprehensive guide. From bold prints to sustainable fashion,...
-              </p>
-              <div className="card-actions">
-                <div className="flex justify-between gap-5 items-center">
+          <div className="bg-white shadow-md rounded-lg overflow-hidden flex flex-col h-96">
+            <div className="h-48 w-full overflow-hidden">
+              <img src="https://www.boxmark.com/0cache/bilder152_900x600_f_center_middle.jpg" className="w-full h-full object-cover" />
+            </div>
+            <div className="p-4 flex-grow flex flex-col">
+              <h2 className="text-lg font-bold mb-2">Summer 2024 Fashion Trends: What to Wear This Season</h2>
+              <p className="flex-grow">Dive into the hottest trends of Summer 2024 with our
+                comprehensive guide. From bold prints to sustainable fashion,...</p>
+            </div>
+            <div className="px-10 mx-auto mb-5 flex justify-between gap-5 items-center">
+              <Link to="/blogDetails">
                 <button className="btn bg-pink-700 text-white hover:text-black  active:bg-red-700 focus:outline-none focus:ring focus:ring-red-300 focus:text-white">
                   Read More
-                  </button>
-                  
-                  <p className="text-sm">15 june, 2024</p>
-                </div>
-              </div>
+                </button>
+              </Link>
+
+              <p className="text-sm">6 May, 2024</p>
             </div>
+
           </div>
         </SwiperSlide>
 
+        <SwiperSlide>
+        <div className="bg-white shadow-md rounded-lg overflow-hidden flex flex-col h-96">
+            <div className="h-48 w-full overflow-hidden">
+              <img src="https://cdn.dribbble.com/userupload/8647640/file/original-4215771ec4810cd6ecf1485df5d55d96.png?resize=400x300&vertical=center" className="w-full h-full object-cover" />
+            </div>
+            <div className="p-4 flex-grow flex flex-col">
+              <h2 className="text-lg font-bold mb-2">Summer 2024 Fashion Trends: What to Wear This Season</h2>
+              <p className="flex-grow">Dive into the hottest trends of Summer 2024 with our
+                comprehensive guide. From bold prints to sustainable fashion,...</p>
+            </div>
+            <div className="px-10 mx-auto mb-5 flex justify-between gap-5 items-center">
+              <Link to="/blogDetails">
+                <button className="btn bg-pink-700 text-white hover:text-black  active:bg-red-700 focus:outline-none focus:ring focus:ring-red-300 focus:text-white">
+                  Read More
+                </button>
+              </Link>
+
+              <p className="text-sm">6 May, 2024</p>
+            </div>
+
+          </div>
+
+
+         
+
+        </SwiperSlide>
+
+        <SwiperSlide>
+        <div className="bg-white shadow-md rounded-lg overflow-hidden flex flex-col h-96">
+            <div className="h-48 w-full overflow-hidden">
+              <img src="https://cache.careers360.mobi/media/presets/860X430/article_images/2020/6/23/Know_all_about_leather_designing.webp" className="w-full h-full object-cover" />
+            </div>
+            <div className="p-4 flex-grow flex flex-col">
+              <h2 className="text-lg font-bold mb-2">Summer 2024 Fashion Trends: What to Wear This Season</h2>
+              <p className="flex-grow">Dive into the hottest trends of Summer 2024 with our
+                comprehensive guide. From bold prints to sustainable fashion,...</p>
+            </div>
+            <div className="px-10 mx-auto mb-5 flex justify-between gap-5 items-center">
+              <Link to="/blogDetails">
+                <button className="btn bg-pink-700 text-white hover:text-black  active:bg-red-700 focus:outline-none focus:ring focus:ring-red-300 focus:text-white">
+                  Read More
+                </button>
+              </Link>
+
+              <p className="text-sm">6 May, 2024</p>
+            </div>
+
+          </div>
+
+
+         
+
+        </SwiperSlide>
 
 
       </Swiper>
