1f5b59fcd21ae084994ac7be2891c2af7e496c91
ashikur <ashikur1602@gmail.com>
2024-07-11 12:44:51 +0600

    Footer mobile view completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
     return (
         <>
 
-            <footer className="grid  grid-cols-2 bg-primary text-neutral-content py-5">
+            <footer className="grid lg:pb-5  grid-cols-2 bg-primary text-neutral-content pt-4 mb-14 lg:mb-0">
 
                 <nav>
                     <div className="grid grid-flow-col gap-4">
@@ -19,30 +19,29 @@ const Footer = () => {
 
                             <img src="https://i.ibb.co/YB1115L/footer-image.png" className="lg:pl-5 lg:w-3/4 w-3/4 mx-auto" alt="" />
 
-                            <nav className="px-5 text-white flex sm:block justify-center items-center">
-                                <div className="flex px-5 sm:px-20 lg:py-5 gap-5 flex-wrap">
-                                    <Link to="https://x.com/"><BiLogoTwitter className="text-sm lg:text-3xl " /></Link>
-                                    <Link to="https://www.youtube.com/"><TbBrandYoutubeFilled className="text-sm lg:text-3xl" />
+                            <nav className="lg:pl-9 pl-6 text-white flex sm:block  items-center">
+                                <div className="flex flex-row px-5 sm:px-20 lg:py-5 lg:gap-5 gap-1 flex-wrap text-[8px] lg:text-3xl">
+                                    <Link to="https://x.com/"><BiLogoTwitter className=" " /></Link>
+                                    <Link to="https://www.youtube.com/"><TbBrandYoutubeFilled className="" />
                                     </Link>
-                                    <Link to="https://www.facebook.com/"><FaFacebook className="text-sm lg:text-3xl" /></Link>
-                                    <Link to="https://www.pinterest.com/"><BsPinterest className="text-sm lg:text-3xl" /></Link>
+                                    <Link to="https://www.facebook.com/"><FaFacebook className="" /></Link>
+                                    <Link to="https://www.pinterest.com/"><BsPinterest className="" /></Link>
                                 </div>
                             </nav>
-                            <div className=" lg:pl-20 text-center sm:text-start" >
-
-
+                            <div className=" lg:pl-20  sm:text-start" >
                                 <div className="lg:px-10">
-                                    <p className="text-sm lg:text-2xl font-bold  text-white px-10 lg:px-0"> House # 3/GA,Shyamoli, Road # 1. Dhaka-1207.</p>
-                                    <p className="text-sm lg:text-2xl font-bold text-white px-10 lg:px-0">Email: info@bifdt.com</p>
+                                    <p className="text-[8px] lg:text-xl lg:font-bold  text-white px-10 lg:px-0"> House # 3/GA,Shyamoli, Road # 1. Dhaka-1207.</p>
+                                    <p className="text-[8px] lg:text-xl lg:font-bold text-white px-10 lg:px-0">Email: info@bifdt.com</p>
                                 </div>
                             </div>
+
                         </div>
                     </div>
                 </nav>
 
                 <div className="text-white">
                     <div className="mx-auto lg:px-9">
-                        <p className="text-sm lg:text-3xl font-bold mt-5 lg:text-center sm:text-start">Contact Number</p>
+                        <p className="text-sm lg:text-3xl font-bold lg:mt-5  sm:text-start">Contact Number</p>
                         <div className="hidden lg:flex justify-center sm:justify-start items-center ">
                             <div className="">
                                 <span className="flex gap-2 my-5"><FaSquarePhone /> 016-29-17-17-17,</span>
@@ -55,13 +54,17 @@ const Footer = () => {
                                 <span className="flex gap-2 my-5"><FaSquarePhone /> 019-79-79-94-94</span>
                             </div>
                         </div>
-                        <div className="lg:hidden pb-14">
-                            <span className="flex text-sm gap-1"><FaSquarePhone /> 016-29-17-17-17,</span>
-                            <span className="flex text-sm gap-1"><FaSquarePhone /> 016-29-18-18-18,</span>
-                            <span className="flex text-sm gap-1"><FaSquarePhone /> 015-52-64-44-79,</span>
-                            <span className="flex text-sm gap-1"><FaSquarePhone /> 019-15-67-05-40,</span>
-                            <span className="flex text-sm gap-1"><FaSquarePhone /> 019-79-79-93-93,</span>
-                            <span className="flex text-sm gap-1"><FaSquarePhone /> 019-79-79-94-94</span>
+                        <div className="lg:hidden flex text-[8px] my-3">
+                            <div>
+                                <span className="flex  gap-1"><FaSquarePhone /> 016-29-17-17-17,</span>
+                                <span className="flex  gap-1"><FaSquarePhone /> 016-29-18-18-18,</span>
+                                <span className="flex  gap-1"><FaSquarePhone /> 015-52-64-44-79,</span>
+                            </div>
+                            <div>
+                                <span className="flex  gap-1"><FaSquarePhone /> 019-15-67-05-40,</span>
+                                <span className="flex  gap-1"><FaSquarePhone /> 019-79-79-93-93,</span>
+                                <span className="flex  gap-1"><FaSquarePhone /> 019-79-79-94-94</span>
+                            </div>
                         </div>
                     </div>
                 </div>
