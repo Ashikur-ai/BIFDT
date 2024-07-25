ed4cc5ef6e4e0a8bbf13d691231a48a6d72390eb
junayet <muhammadjunayetmaruf@gmail.com>
2024-07-25 11:27:18 +0300

    course category and course table has become responsive

--- src/pages/ClientSide/course/CourseDetailsTab.jsx
+++ src/pages/ClientSide/course/CourseDetailsTab.jsx
@@ -4,9 +4,16 @@ import 'react-tabs/style/react-tabs.css';
 import { Component } from 'react';
 import Faq from 'react-faq-component';
 import { Grommet, Tab, Tabs } from 'grommet';
+import { useQuery } from '@tanstack/react-query';
+import { useParams } from 'react-router-dom';
+import useAxiosPublic from '../../../hooks/useAxiosPublic';
+import SemesterTable from '../../../components/SemesterTable';
+import CourseCategory from '../../../components/CourseCategory';
 
 
 const CourseDetailsTab = () => {
+    const { id } = useParams();
+    const axiosPublic = useAxiosPublic()
     const [TabName, setTabName] = useState('2nd')
     const [rows, setRowsOption] = useState(null)
     const customTheme = {
@@ -41,16 +48,42 @@ const CourseDetailsTab = () => {
           `,
         },
     };
+    const { data: courseSemesters = [], isLoading: courseSemestersIsLoading } = useQuery({
+        queryKey: ['singleCourseIdForSemester', id],
+        queryFn: async () => {
+            const res = await axiosPublic.get(`/semesterByCourse/course/${id}`)
+            return res?.data
+        }
+    })
+    const { data: courseCategories = [], isLoading: courseCategoriesIsLoading } = useQuery({
+        queryKey: ['singleCourseId', id],
+        queryFn: async () => {
+            const res = await axiosPublic.get(`/courseCategory/course/${id}`)
+            return res?.data
+        }
+    })
     useEffect(() => {
         rows && rows[0].expand()
-    }, [ rows])
+    }, [rows])
+    useEffect(() => {
+        if (courseCategories?.length > 0) {
+            setTabName(courseCategories[0]?._id)
+        } else {
+            setTabName('4th')
+        }
+    }, [courseCategories])
+    if (courseSemestersIsLoading || courseCategoriesIsLoading) {
+        return ''
+    }
+    console.log(courseCategories);
+    const showingCategory = courseCategories?.find(category => category?._id === TabName) || {}
     const data = {
         title: "",
         rows: [
             {
                 title: <p className='font-bold'>Fashion Designer</p>,
                 content: "Fashion designers are involved from conception to completion in the development and production of clothing lines. While some designers work their way up in the industry through on-the-job experience, nowadays a formal education is often the minimum requirement for this position. Designers must have in-depth knowledge of fashion design concepts, sewing, apparel design, and garment construction, and be familiar with different types of textiles and industry trends. Most designers specialize in areas such as women's clothing, children's wear, or casual wear.",
-                
+
             },
             {
                 title: <p className='font-bold'>Assistant Designer</p>,
@@ -98,18 +131,12 @@ const CourseDetailsTab = () => {
         <div className='overflow-x-hidden'>
             <Grommet Grommet theme={customTheme}>
                 <Tabs justify="start">
-
-
-                    <Tab className='text-red-500' title={<div className={`${btnStyle} ${TabName === '2nd' ? 'font-bold bg-primary' : 'bg-primary/80'}`}>
-                        <p className='transition-all duration-300 '>1-year Course <br /> 12-Month</p>
-                    </div>} onClick={() => setTabName('2nd')}>
-                    </Tab>
-
-                    <Tab className='text-red-500' title={
-                        <div
-                            className={`${btnStyle} ${TabName === '3rd' ? 'font-bold bg-primary' : 'bg-primary/80'}`}><p className='tabBtnText transition-all duration-300'
-                            >6-Month Course</p></div>
-                    } onClick={() => setTabName('3rd')}></Tab>
+                    {
+                        courseCategories?.map(category => <Tab key={category?._id} className='text-red-500' title={<div className={`${btnStyle} ${TabName === category?._id ? 'font-bold bg-primary' : 'bg-primary/80'}`}>
+                            <p className='transition-all duration-300 '>{category.duration}</p>
+                        </div>} onClick={() => setTabName(category?._id)}>
+                        </Tab>)
+                    }
 
                     <Tab className='text-red-500' title={<div className={`${btnStyle} ${TabName === '4th' ? 'font-bold bg-primary' : 'bg-primary/80'}`}><p className='tabBtnText transition-all duration-300'>Semester Details</p></div>} onClick={() => setTabName('4th')}></Tab>
                     <Tab className='text-red-500' title={<div className={`${btnStyle} ${TabName === '1st' ? 'font-bold bg-primary' : 'bg-primary/80'}`}><p className='tabBtnText transition-all duration-300'>Career objective in <br /> fashion design</p></div>} onClick={() => setTabName('1st')}></Tab>
@@ -119,171 +146,14 @@ const CourseDetailsTab = () => {
             {TabName === '1st' && <div className='pl-2  '>
                 <Faq getRowOptions={setRowsOption} config={config} data={data} />
             </div>}
-            {TabName === '2nd' && <div className='pb-10'>
-                <div className="overflow-x-auto w-full ">
-                    <table className="table overflow-hidden w-[800px] mx-auto text-base bg-primary/30">
-                        {/* head */}
-                        <thead className='bg-primary/90 text-white text-base '>
-                            <tr>
-                                <th>Course Title</th>
-                                <th>:</th>
-                                <th className="w-[570px] whitespace-normal">1 Year Diploma in Apparel Merchandising & Buying Management (Normal Credits System)</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {/* row 1 */}
-                            <tr className="">
-                                <th>Course Duration</th>
-                                <td>:</td>
-                                <td>12-Month Theoretical & Practical classes for (Woven, Knit & Sweater) Consolidated by Merchandising, Production planning, Textile science , Industrial Engineering, Types of costing in garment industry, Commercial , Internet & Buyer Sourcing,  Pattern Top, Pattern Bottom, Garments Construction, Fully Fashion, CAD Design. Spoken English, Computer training.  followed by Factory Visit.  followed by Factory Visit.</td>
-                            </tr>
-                            {/* row 2 */}
-                            <tr className='bg-gray-300'>
-                                <th> Regular Batch</th>
-                                <td>:</td>
-                                <td>3/4 days in a week for general students, morning, afternoon, evening and various times of the week..</td>
-                            </tr>
-                            {/* row 3 */}
-                            <tr>
-                                <th>Executive Batch </th>
-                                <td>:</td>
-                                <td>3 days in a week including Friday and Saturday, and evening classes  are available for working people.</td>
-                            </tr>
-                            <tr className='bg-gray-300'>
-                                <th></th>
-                                <td></td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <th>Total Classes</th>
-                                <td>:</td>
-                                <td>Minimum total-144/192 Classes. Theoretical class(1.5-2) Hours, Practical class(2-3) Hours. Total 30 Credits.            ( Normal Credits System )</td>
-                            </tr>
-                            <tr className='bg-gray-300'>
-                                <th>Qualification</th>
-                                <td>:</td>
-                                <td>H.S.C/Bachelor's /Master's  (Any age)</td>
-                            </tr>
-                            <tr>
-                                <th>Course Fee</th>
-                                <td>:</td>
-                                <td>Total payable TK 54,000/=.
-                                    At the time of Admission, TK 6,000/= and the rest will  have to be paid Monthly TK 4,000/= × 12 Installments = TK 48000/=.</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>}
-            {TabName === '3rd' && <div>
-                <div className='pl-2  '>
-
-                    <div className='pl-2  '>
-                        <p className='font-bold text-xl pb-2'>6-Month Certificate Course in Fashion Design.</p>
-                        <div className='space-y-3'>
-                            <div className='flex gap-4'>
-                                <div className='w-1/4'>Course Title :</div>
-                                <div className='w-3/4'>6-Month Certificate Course in Fashion Design.</div>
-                            </div>
-                            <div className='flex gap-4'>
-                                <div className='w-1/4'>Course Duration :</div>
-                                <div className='w-3/4'>6-Month theoretical & practical classes in this Institute. 16 Credits.<span className='font-bold'> Minimum Total -72/96 Classes</span>
-                                    <p>Theoretical class(1.5-2)Hours, Practical class(2-3)Hours. 3/4 days in a week.</p>
-                                </div>
-                            </div>
-                            <div className='flex gap-4'>
-                                <div className='w-1/4'>Course Fee :</div>
-                                <div className='w-3/4'>Total payable TK 30,000/=. At the time of Admission, TK 6,000/= and the rest will
-                                    have to be paid Monthly TK 4,000/= × 6 Installments = TK 24,000/=</div>
-                            </div>
-                        </div>
-
-                    </div>
-
-                </div>
-            </div>}
+            {
+                showingCategory?.name && <CourseCategory category={showingCategory} />
+            }
             {TabName === '4th' && <div>
                 <div>
-                    <div className="overflow-x-auto max-w-[800px] mx-auto">
-                        <table className="min-w-full border-collapse border border-gray-200 bg-primary/20 ">
-                            <thead>
-                                <tr className="bg-primary/90 text-white">
-                                    <th colSpan="2" className="border border-gray-200 px-4 py-2 text-left">First Semester</th>
-                                    <th className="border border-gray-200 px-4 py-2 text-left">Credits</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr className=''>
-                                    <td className="px-4 py-2 border-r-2 border-0 border-black border-b-[1.5px] border-b-primary border-l-[2px]">1</td>
-                                    <td className="border-0 border-r-2 border-black px-4 py-2 border-b-[1.5px] border-b-primary">Production & Designing Department</td>
-                                    <td className="px-4 py-2 border-r-2 border-0 border-black border-b-[1.5px] border-b-primary border-l-[2px]">2 Credits</td>
-                                </tr>
-                                <tr className=" ">
-                                    <td className="px-4 py-2 border-r-2 border-0 border-black border-b-[1.5px] border-b-primary border-l-[2px]">2</td>
-                                    <td className="border-0 border-r-2 border-black px-4 py-2 border-b-[1.5px] border-b-primary">Textile Department</td>
-                                    <td className="px-4 py-2 border-r-2 border-0 border-black border-b-[1.5px] border-b-primary border-l-[2px]">2 Credits</td>
-                                </tr>
-                                <tr>
-                                    <td className="px-4 py-2 border-r-2 border-0 border-black border-b-[1.5px] border-b-primary border-l-[2px]">3</td>
-                                    <td className="border-0 border-r-2 border-black px-4 py-2 border-b-[1.5px] border-b-primary">Business Communication (Spoken) Department</td>
-                                    <td className="px-4 py-2 border-r-2 border-0 border-black border-b-[1.5px] border-b-primary border-l-[2px]">2 Credits</td>
-                                </tr>
-                                <tr className=" ">
-                                    <td className="px-4 py-2 border-r-2 border-0 border-black border-b-[1.5px] border-b-primary border-l-[2px]">4</td>
-                                    <td className="border-0 border-r-2 border-black px-4 py-2 border-b-[1.5px] border-b-primary">Commercial Department</td>
-                                    <td className="px-4 py-2 border-r-2 border-0 border-black border-b-[1.5px] border-b-primary border-l-[2px]">1 Credit</td>
-                                </tr>
-                                <tr>
-                                    <td className="px-4 py-2 border-r-2 border-0 border-black border-b-[1.5px] border-b-primary border-l-[2px]">5</td>
-                                    <td className="border-0 border-r-2 border-black px-4 py-2 border-b-[1.5px] border-b-primary">Industrial Engineering Department</td>
-                                    <td className="px-4 py-2 border-r-2 border-0 border-black border-b-[1.5px] border-b-primary border-l-[2px]">2 Credits</td>
-                                </tr>
-                                <tr className=" ">
-                                    <td className="px-4 py-2 border-r-2 border-0 border-black border-b-[1.5px] border-b-primary border-l-[2px]">6</td>
-                                    <td className="border-0 border-r-2 border-black px-4 py-2 border-b-[1.5px] border-b-primary">Merchandising Department</td>
-                                    <td className="px-4 py-2 border-r-2 border-0 border-black border-b-[1.5px] border-b-primary border-l-[2px]">2 Credits</td>
-                                </tr>
-                                <tr>
-                                    <td className="px-4 py-2 border-r-2 border-0 border-black border-b-[1.5px] border-b-primary border-l-[2px]">7</td>
-                                    <td className="border-0 border-r-2 border-black px-4 py-2 border-b-[1.5px] border-b-primary">Internet & Buyer Sourcing Department</td>
-                                    <td className="px-4 py-2 border-r-2 border-0 border-black border-b-[1.5px] border-b-primary border-l-[2px]">1 Credit</td>
-                                </tr>
-                                <tr className="bg-gray-100">
-                                    <th colSpan="2" className="border border-gray-200 px-4 py-2 text-left">Second Semester</th>
-                                    <th className="border border-gray-200 px-4 py-2 text-left">Credits</th>
-                                </tr>
-                                <tr>
-                                    <td className="px-4 py-2 border-r-2 border-0 border-black border-b-[1.5px] border-b-primary border-l-[2px]">8</td>
-                                    <td className="border-0 border-r-2 border-black px-4 py-2 border-b-[1.5px] border-b-primary">Computer Fundamental Department</td>
-                                    <td className="px-4 py-2 border-r-2 border-0 border-black border-b-[1.5px] border-b-primary border-l-[2px]">3 Credits</td>
-                                </tr>
-                                <tr className=" ">
-                                    <td className="px-4 py-2 border-r-2 border-0 border-black border-b-[1.5px] border-b-primary border-l-[2px]">9</td>
-                                    <td className="border-0 border-r-2 border-black px-4 py-2 border-b-[1.5px] border-b-primary">Basic Pattern Department (1) Top Variation</td>
-                                    <td className="px-4 py-2 border-r-2 border-0 border-black border-b-[1.5px] border-b-primary border-l-[2px]">4 Credits</td>
-                                </tr>
-                                <tr>
-                                    <td className="px-4 py-2 border-r-2 border-0 border-black border-b-[1.5px] border-b-primary border-l-[2px]">10</td>
-                                    <td className="border-0 border-r-2 border-black px-4 py-2 border-b-[1.5px] border-b-primary">Basic Pattern Department (2) Bottom Variation</td>
-                                    <td className="px-4 py-2 border-r-2 border-0 border-black border-b-[1.5px] border-b-primary border-l-[2px]">4 Credits</td>
-                                </tr>
-                                <tr className=" ">
-                                    <td className="px-4 py-2 border-r-2 border-0 border-black border-b-[1.5px] border-b-primary border-l-[2px]">11</td>
-                                    <td className="border-0 border-r-2 border-black px-4 py-2 border-b-[1.5px] border-b-primary">Garments Construction Department</td>
-                                    <td className="px-4 py-2 border-r-2 border-0 border-black border-b-[1.5px] border-b-primary border-l-[2px]">2 Credits</td>
-                                </tr>
-                                <tr>
-                                    <td className="px-4 py-2 border-r-2 border-0 border-black border-b-[1.5px] border-b-primary border-l-[2px]">12</td>
-                                    <td className="border-0 border-r-2 border-black px-4 py-2 border-b-[1.5px] border-b-primary">Merchandising Department (2)</td>
-                                    <td className="px-4 py-2 border-r-2 border-0 border-black border-b-[1.5px] border-b-primary border-l-[2px]">3 Credits</td>
-                                </tr>
-                                <tr>
-                                    <td className="px-4 py-2 border-r-2 border-0 border-black border-b-[1.5px] border-b-primary border-l-[2px]"></td>
-                                    <td className="border-0 border-r-2 border-black px-4 py-2 border-b-[1.5px] border-b-primary">Total</td>
-                                    <td className="px-4 py-2 border-r-2 border-0 border-black border-b-[1.5px] border-b-primary border-l-[2px]">30 Credits</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
+                    {
+                        courseSemesters.map(semester => <SemesterTable key={semester._id} semesterTitle={semester?.semesterTitle} subjects={semester?.subjects} />)
+                    }
                 </div>
             </div>}
             {/* <Tabs>
