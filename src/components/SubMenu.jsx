881b58ab6261d0d56eb97a21fba02c8a05e04709
ashikur <ashikur1602@gmail.com>
2024-07-27 16:44:57 +0600

    logo text added

--- src/components/SubMenu.jsx
+++ src/components/SubMenu.jsx
@@ -6,37 +6,18 @@ import { Link } from 'react-router-dom';
 const SubMenu = () => {
     return (
         <>
-         <div className="bg-white flex w-3/4 mx-auto my-2">
-                <div className='flex flex-col lg:flex-row mx-auto w-3/4 items-center pt-1'>
-                    <div className="flex flex-col lg:flex-row items-center gap-1">
-                        <p className="flex items-center gap-1"><FaPhoneAlt />Call Us!:</p>
-                        <div className="lg:flex">
-                            <a className="font-bold" href={`tel:01629-171717`}>
-                                <p>+8801629-171717</p>
-                            </a>
-                            <a className="lg:ml-2 font-bold" href={`tel:01629-181818`}>
-                                <p>+8801629-181818</p>
-                            </a>
-                        </div>
-                    </div>
-                    <p className="lg:ml-5 flex items-center gap-2">
-                        <MdEmail />
-                        <span>info@bifdt.com</span>
-                    </p>
-                </div>
-                <div className='hidden lg:flex gap-5 '>
+            <div className="bg-orange-700  py-2 text-white shadow-2xl hidden lg:flex">
+                <div className='flex gap-5 w-10/12 mx-auto'>
                     <Link to="/onlineAdmission">
-                        <button className="lg:p-2 p-1 rounded-md bg-primary text-white hover:font-bold  hover:scale-110 duration-300 transition-all active:bg-white-700 focus:outline-none focus:ring focus:ring-red-300 focus:text-white w-max mr-9 lg:mr-0">
-                            Online Admission
-                        </button>
+                        Online Admission
                     </Link>
+
                     <Link to="/freeSeminar">
-                        <button className="lg:p-2 p-1 rounded-md bg-primary text-white hover:font-bold hover:scale-110 duration-300 transition-all  active:bg-white-700 focus:outline-none focus:ring focus:ring-red-300 focus:text-white w-max mr-9 lg:mr-0">
-                            Free Seminar
-                        </button>
+                        Free Seminar
                     </Link>
                 </div>
-            </div>   
+
+            </div>
         </>
     );
 };
