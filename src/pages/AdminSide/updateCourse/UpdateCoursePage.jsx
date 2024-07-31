2c25ebb777f0be27b29fb669b2ae3930241efcaa
junayet <muhammadjunayetmaruf@gmail.com>
2024-07-31 11:35:31 +0300

    course details page has updated

--- src/pages/AdminSide/updateCourse/UpdateCoursePage.jsx
+++ src/pages/AdminSide/updateCourse/UpdateCoursePage.jsx
@@ -205,14 +205,14 @@ const UpdateCoursePage = () => {
                                             {/* title */}
                                             <div className="p-2 w-full">
                                                 <div className="relative">
-                                                    <label className="leading-7 text-sm text-gray-600 font-bold">Course Title</label>
+                                                    <label className="leading-7 text-sm text-gray-600 font-bold">Course Category</label>
                                                     <input defaultValue={courseData.title} required type="text" name="courseTitle" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                                                 </div>
                                             </div>
                                             {/* subtitle */}
                                             <div className="p-2 w-full">
                                                 <div className="relative">
-                                                    <label className="leading-7 text-sm text-gray-600 font-bold">Course Subtitle</label>
+                                                    <label className="leading-7 text-sm text-gray-600 font-bold">Course Title</label>
                                                     <input defaultValue={courseData.subtitle} required type="text" id="title" name="subtitle" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                                                 </div>
                                             </div>
