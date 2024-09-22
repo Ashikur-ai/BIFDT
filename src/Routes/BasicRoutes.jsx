f29fc8311e5b6b35b2814545f2a1a1ee4c4ea380
ashikur <ashikur1602@gmail.com>
2024-09-22 10:42:25 +0600

    Dashboard changed

--- src/Routes/BasicRoutes.jsx
+++ src/Routes/BasicRoutes.jsx
@@ -108,7 +108,7 @@ const BasicRoutes = createBrowserRouter([
     },
     {
         path: "/dashboard",
-        element: <PrivateRouts><DashboardLayout></DashboardLayout></PrivateRouts>,
+        element: <DashboardLayout></DashboardLayout>,
         children: [
             {
                 path: "/dashboard",
